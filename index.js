889e8db0a70b3e08449eb88c8b2d63348f0b74c7
Dawa Sherpa <56500281+dawasherpa-ui@users.noreply.github.com>
2023-12-29 14:54:44 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express=require("express")
+const generator =require("generate-password")
+const bodyParser = require('body-parser')
+const app=express()
+const cors = require('cors');
+const PORT=3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors({
+    origin: 'http://localhost:5173/'
+  }));
+app.get("/",(req,res)=>{
+    res.send({msg:"Hello World"})
+})
+app.post("/",async(req,res)=>{
+console.log(req.body)
+try{
+const {length,number,symbol,lowercase,uppercase,exsimlar,exclude,strict,multiple}=req.body
+let pass;
+if(multiple){
+    pass=generator.generateMultiple(multiple,{
+        length:length||10,
+        numbers: number || false,
+        symbols : symbol  || false ,
+        lowercase : lowercase || false,
+        uppercase : uppercase || false,
+        excludeSimilarCharacters:exsimlar || false,
+        exclude : exclude || [],
+        strict : strict || false
+    })
+    console.log(pass);
+    res.json([{password:pass}])
+}
+else{
+pass=generator.generate({
+    length:length||10,
+    numbers: number || false,
+    symbols : symbol  || false ,
+    lowercase : lowercase || false,
+    uppercase : uppercase || false,
+    excludeSimilarCharacters:exsimlar || false,
+    exclude : exclude || [],
+    strict : strict || false
+})
+console.log(pass);
+res.json([{password:pass}])
+}
+}
+catch(err){
+    console.log(err.message)
+    res.json([{error:err.message}])
+}
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running in port ",PORT)
+})
